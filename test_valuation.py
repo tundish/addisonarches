a5a9358f5de7ac43b7c0c1746573c8fb4a8029bb
Tundish <tundish@thuswise.org>
2015-05-07 16:00:44 +0100

    Sketching API of ValueBook.

--- test_valuation.py
+++ test_valuation.py
@@ -16,39 +16,56 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import defaultdict
 from collections import deque
 from collections import namedtuple
 import datetime
 from decimal import Decimal
+import functools
 import unittest
 
 from tallywallet.common.finance import Note
+from tallywallet.common.finance import value_simple
 
 
-Asset = namedtuple("Asset", ["quantity", "acquired"])
-Valuation = namedtuple("Valuation", [])
+Commodity = namedtuple("Commodity", ["label", "description"])
+Asset = namedtuple(
+    "Asset",
+    ["commodity", "unit", "quantity", "acquired"]
+)
 
-class Valuation:
+Offer = namedtuple("Offer", ["ts"])
+Valuation = namedtuple("Valuation", ["ts"])
 
-    def __init__(self, note):
+class ValueBook(dict):
+
+    def commit(self, asset:Asset, obj:[Note, Offer]):
         """
         Valuation remains constant during duration of note.
         Then each Offer or Valuation modulates the mean of
         the series.
         """
-        self.note = note
-        self.series = deque(maxlen=8)  # TODO: calculate maxlen
+        if isinstance(obj, Note):
+            self.series = deque(maxlen=8)  # TODO: calculate maxlen
+        else:
+            raise NotImplementedError
 
 
 class ValuationAPITests(unittest.TestCase):
 
     def test_class(self):
+        then = datetime.date(2015, 4, 1)
         note = Note(
-            date=datetime.date(1995, 5, 11),
+            date=then,
             principal=1500,
-            currency=None,
-            term=datetime.timedelta(days=90),
-            interest=Decimal("0.08"),
-            period=datetime.timedelta(days=360)
+            currency="£",
+            term=datetime.timedelta(days=30),
+            interest=Decimal("0.20"),
+            period=datetime.timedelta(days=30)
         )
-        self.fail(note)
+        goods = Commodity("VCRs", "Betamax video cassette recorders")
+        book = ValueBook()
+        val = book.commit(Asset(goods, int, 10, note.date), note)
+        #val = book.commit(Asset(goods, int, 10, note.date), Offer(..))
+        self.fail(book)
+        self.fail(value_simple(note))
