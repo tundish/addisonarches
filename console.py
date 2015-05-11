675e834974b0f7c19e9a5ff49d149e3bfb88560a
Tundish <tundish@thuswise.org>
2015-05-11 13:46:56 +0100

    Quick play with console module.

---
+++ console.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#   -*- encoding: UTF-8 -*-
+
+# This file is part of Addison Arches.
+#
+# Addison Arches is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Addison Arches is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
+
+import cmd
+from collections import namedtuple
+import datetime
+import itertools
+
+Location = namedtuple("Location", ["name", "capacity"])
+
+locations = [
+    Location("Indigent Street Car Boot Sale", 1000),
+    Location("Harry's House Clearances", 300),
+    Location("Kinh Ship Bulk Buy", 500),
+    Location("Addison Arches 18a", 100),
+]
+
+
+class Console(cmd.Cmd):
+    clock = (
+        t for t in (
+            datetime.datetime(year=2015, month=5, day=11) +
+            datetime.timedelta(seconds=i)
+            for i in itertools.count(0, 30 * 60))
+        if 8 <= t.hour <= 19)
+    prompt = "{:%A %H:%M} > ".format(next(clock))
+
+    def postcmd(self, stop, line):
+        self.prompt = "{:%A %H:%M} > ".format(next(self.clock))
+        return stop
+
+if __name__ == "__main__":
+    console = Console()
+    console.cmdloop()
