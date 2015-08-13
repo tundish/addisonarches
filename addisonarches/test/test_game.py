ce8a4734f2593727183d1207b6f94961d710ae31
Tundish <tundish@thuswise.org>
2015-08-13 13:54:46 +0100

    Sketching out a test class for Game.

--- addisonarches/test/test_game.py
+++ addisonarches/test/test_game.py
@@ -16,146 +16,66 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
 
-import glob
+import asyncio
 import os.path
-import pickle
 import tempfile
 import unittest
 
+from addisonarches.game import Clock
 from addisonarches.game import Game
 from addisonarches.game import Persistent
 import addisonarches.scenario
 
-
-class PersistentTests(unittest.TestCase):
-
-    user = "someone@somewhere.net"
-
-    def make_home(self):
-        path = Persistent.Path(self.root.name, PersistentTests.user, None, None)
-        return Persistent.make_path(path)
-
-    def make_slot(self):
-        path = Persistent.Path(
-            self.root.name, PersistentTests.user, None, "business.pkl")
-        return Persistent.make_path(path)
-
-    def setUp(self):
-        self.root = tempfile.TemporaryDirectory()
-        self.assertTrue(os.path.isdir(self.root.name))
-
-    def tearDown(self):
-        if os.path.isdir(self.root.name):
-            self.root.cleanup()
-        self.assertFalse(os.path.isdir(self.root.name))
-        self.root = None
-
-    def test_make_home(self):
-        path = self.make_home()
-        self.assertTrue(path)
-        self.assertTrue(path.root)
-        self.assertTrue(path.home)
-        self.assertFalse(path.slot)
-        self.assertFalse(path.file)
-        self.assertTrue(
-            os.path.isdir(os.path.join(*(i for i in path if i is not None)))
-        )
- 
-    def test_make_home_twice(self):
-        first = self.make_home()
-        path = self.make_home()
-        self.assertEqual(first, path)
-        self.assertTrue(path)
-        self.assertTrue(path.root)
-        self.assertTrue(path.home)
-        self.assertFalse(path.slot)
-        self.assertFalse(path.file)
-        self.assertTrue(
-            os.path.isdir(os.path.join(*(i for i in path if i is not None)))
-        )
-    
-    def test_make_slot(self):
-        path = self.make_slot()
-        self.assertTrue(path)
-        self.assertTrue(path.root)
-        self.assertTrue(path.home)
-        self.assertTrue(path.slot)
-        self.assertTrue(path.file)
-        self.assertNotIn(path.root, path.slot)
-        self.assertNotIn(path.root, path.file)
-        self.assertTrue(
-            os.path.isdir(os.path.join(*path[:-1]))
-        )
-         
-    def test_make_slot_twice(self):
-        first = self.make_slot()
-        path = self.make_slot()
-        self.assertNotEqual(first, path)
-        self.assertTrue(path)
-        self.assertTrue(path.root)
-        self.assertTrue(path.home)
-        self.assertTrue(path.slot)
-        self.assertTrue(path.file)
-        self.assertTrue(
-            os.path.isdir(os.path.join(*path[:-1]))
-        )
-         
-    def test_no_recent_slot(self):
-        path = self.make_home()
-        path = Persistent.recent_slot(path)
-        self.assertIs(None, path.slot)
-        path = path._replace(file="objects.pkl")
-        path = Persistent.make_path(path)
-         
-    def test_recent_slot(self):
-        path = self.make_slot()
-        self.assertEqual(
-            path.slot,
-            Persistent.recent_slot(path).slot
-        )
-         
 class GameTests(unittest.TestCase):
 
     user = "someone@somewhere.net"
 
-    def setUp(self):
-        self.root = tempfile.TemporaryDirectory()
-        self.assertTrue(os.path.isdir(self.root.name))
-
-    def test_pickling_businesses(self):
-        path = Persistent.Path(self.root.name, GameTests.user, None, None)
-        Persistent.make_path(path)
+    @staticmethod
+    def create_experts(parent, loop=None):
         options = Game.options(
             Game.Player(GameTests.user, "Player 1"),
-            parent=self.root.name
+            parent=parent
         )
         game = Game(
             Game.Player(GameTests.user, "Player 1"),
             addisonarches.scenario.businesses,
+            loop=loop,
             **options
         )
-        nBusinesses = len(game.businesses)
         game.load()
-        self.assertEqual(nBusinesses + 1, len(game.businesses))  # Player added
 
-        game.declare({"businesses": False})
-        self.assertEqual(
-            0,
-            len(glob.glob(os.path.join(path.root, path.home, '*', "businesses.pkl")))
-        )
+        options = Clock.options(parent=parent)
+        clock = Clock(loop=loop, **options)
+        return (clock, game)
 
-        game.declare({"businesses": game.businesses})
-        pickled = glob.glob(os.path.join(path.root, path.home, '*', "businesses.pkl"))
-        self.assertEqual(1, len(pickled))
+    def setUp(self):
+        self.root = tempfile.TemporaryDirectory()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
 
-        with open(pickled[0], "rb") as fObj:
-            businesses = pickle.load(fObj)
-        self.assertEqual(len(game.businesses), len(businesses))
-        self.assertEqual(
-            "Addison Arches 18a",
-            list(businesses[0].inventories.keys())[0]
-        )
-        
+        path = Persistent.Path(self.root.name, GameTests.user, None, None)
+        Persistent.make_path(path)
+
+    def test_go(self):
+        #tasks = [
+        #    asyncio.Task(clock(loop=loop)),
+        #    asyncio.Task(game(loop=loop)),
+        #]
+        @asyncio.coroutine
+        def stimulus(q):
+            # Collision id, actor, stage
+            obj = (id(None), uuid.uuid4().hex, uuid.uuid4().hex)
+            yield from q.put(obj)
+            yield from q.put(None)
+
+        q = asyncio.Queue(loop=self.loop)
+        clock, game = GameTests.create_experts(
+            self.root.name, loop=self.loop)
+        self.loop.run_until_complete(asyncio.wait(
+            [stimulus(q), clock, game],
+            loop=self.loop, timeout=1
+        ))
+
     def tearDown(self):
         if os.path.isdir(self.root.name):
             self.root.cleanup()
