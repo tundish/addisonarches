d4a0b0426235656ea45050f19b7a643594435688
Tundish <tundish@thuswise.org>
2015-10-27 13:57:03 +0000

    Starting to work on tests for templates.

--- addisonarches/web/hateoas.py
+++ addisonarches/web/hateoas.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python3
-# encoding: UTF-8
+#!/usr/bin/env python
+#   -*- encoding: UTF-8 -*-
 
-from collections import OrderedDict
+# This file is part of Addison Arches.
+#
+# Addison Arches is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Addison Arches is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from collections import namedtuple
 import functools
-from math import ceil
-from math import log10
-
-from cloudhands.common.types import NamedDict
-from cloudhands.common.types import NamedList
-
 
 Action = namedtuple(
     "Action", ["name", "rel", "typ", "ref", "method", "parameters", "prompt"])
@@ -33,52 +42,3 @@ class Validating:
             i for i in self.parameters
             if i.name in self and not i.regex.match(self[i.name])]
         return missing
-
-
-class Contextual:
-
-    def configure(self, session=None, user=None):
-        return self
-
-
-class Region(NamedList):
-
-    @staticmethod
-    def present(obj):
-        return None
-
-    def push(self, obj, session=None, user=None, **kwargs):
-        view = self.__class__.present(obj, **kwargs)
-        if session and isinstance(view, Contextual):
-            view.configure(session, user)
-        if view:
-            self.append(view)
-        return view
-
-
-class PageBase:
-
-    plan = []
-
-    def __init__(self, session=None, user=None, paths={}):
-        Layout = namedtuple("Layout", [n for n, c in self.plan])
-        self.layout = Layout(
-            **{name: typ().name(name) for name, typ in self.plan})
-
-        # Bake in session and user to regions
-        for region in self.layout:
-            region.push = functools.partial(
-                region.push, session=session, user=user)
-
-    def termination(self, **kwargs):
-        for region in self.layout:
-            size = kwargs.get(region.name, len(region))
-            for n, facet in enumerate(region):
-                try:
-                    template = "{{:0{0}}}_{{:0{0}}}".format(ceil(log10(size)))
-                    facet.name(template.format(n + 1, size))
-                except TypeError:
-                    continue
-
-            yield (region.name,
-                   OrderedDict([(facet.name, facet) for facet in region]))
--- addisonarches/web/test/test_hateoas.py
+++ addisonarches/web/test/test_hateoas.py
@@ -8,139 +8,34 @@ import re
 import unittest
 import uuid
 
-
 import pkg_resources
+import pyratemp
 
-from cloudhands.common.types import NamedDict
+from addisonarches.web.hateoas import Action
+from addisonarches.web.hateoas import Parameter
+from addisonarches.web.utils import TemplateLoader
 
-from cloudhands.web.hateoas import Action
-from cloudhands.web.hateoas import PageBase
-from cloudhands.web.hateoas import Parameter
-from cloudhands.web.hateoas import Region
-
-"""
-info
-====
-
-self: The 'self' link for the primary object
-names: A mapping of object types to URLS (even python://collections.abc.Sequence)
-paths: A mapping of root paths to URLS
-versions: Versions of backend packages
-
-nav
-===
-
-Links to other objects with a relationship
-
-"""
-
-item_macro = PageTemplate(pkg_resources.resource_string(
-    "cloudhands.web.templates", "item_list.pt"))
-nav_macro = PageTemplate(pkg_resources.resource_string(
-    "cloudhands.web.templates", "nav_list.pt"))
-option_macro = PageTemplate(pkg_resources.resource_string(
-    "cloudhands.web.templates", "option_list.pt"))
+#item_macro = PageTemplate(pkg_resources.resource_string(
+#    "cloudhands.web.templates", "item_list.pt"))
+item_macro = pyratemp.Template(
+    filename="items.prt",
+    loader_class=TemplateLoader
+)
+#nav_macro = PageTemplate(pkg_resources.resource_string(
+#    "cloudhands.web.templates", "nav_list.pt"))
+#option_macro = PageTemplate(pkg_resources.resource_string(
+#    "cloudhands.web.templates", "option_list.pt"))
 
 Ownership = namedtuple("Ownership", ["uuid", "limit", "level"])
 SimpleType = namedtuple("SimpleType", ["uuid", "name"])
 
 
-class TypesEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        try:
-            return obj.pattern
-        except AttributeError:
-            return json.JSONEncoder.default(self, obj)
-
-class ObjectView(NamedDict):
-
-    @property
-    def public(self):
-        return ["name"]
-
-
-class OwnershipView(NamedDict):
-
-    @property
-    def public(self):
-        return ["level"]
-
-
-class InfoRegion(Region):
-
-    @singledispatch
-    def present(obj):
-        return None
-
-
-class ItemsRegion(Region):
-
-    @singledispatch
-    def present(obj):
-        return None
-
-    @present.register(SimpleType)
-    def present_objects(obj):
-        item = {k: getattr(obj, k) for k in ("uuid", "name")}
-        item["_links"] = [
-            Action(obj.name, "canonical", "/object/{}", obj.uuid,
-            "post", [], "View")]
-        return ObjectView(item)
-
-
-class NavRegion(Region):
-
-    @singledispatch
-    def present(obj):
-        return None
-
-    @present.register(SimpleType)
-    def present_objects(obj, isSelf=False):
-        item = {k: getattr(obj, k) for k in ("uuid", "name")}
-        rel = "self" if isSelf else "canonical"
-        item["_links"] = [
-            Action(obj.name, rel, "/object/{}", obj.uuid,
-            "get", [], "View")]
-        return ObjectView(item)
-
-
-class OptionsRegion(Region):
-
-    @singledispatch
-    def present(obj):
-        return None
-
-    @present.register(Ownership)
-    def present_objects(obj):
-        item = vars(obj)
-        item["_links"] = [
-            # NB: Parameters drawn from View object if validating
-            Action("New object", "create-form", "/bag", None, "post",
-            [Parameter("name", True, re.compile("\\w{8,128}$"), [], "")],
-            "Create")]
-        return OwnershipView(item)
-
-
-class PlainRegion(Region):
-
-    @singledispatch
-    def present(obj):
-        return None
-
-    @present.register(SimpleType)
-    def present_objects(obj):
-        item = {k: getattr(obj, k) for k in ("uuid", "name")}
-        return ObjectView(item)
-
-
 class TestFundamentals(unittest.TestCase):
 
-    class TestPage(PageBase):
+    def test_items_macro(self):
+        print(item_macro(items=[]))
 
-        plan = [("items", PlainRegion)]
-
-    def test_views_without_links_are_not_displayed(self):
+    def tost_views_without_links_are_not_displayed(self):
         objects = [
             SimpleType(uuid.uuid4().hex, "object-{:03}".format(n))
             for n in range(6)]
@@ -151,14 +46,12 @@ class TestFundamentals(unittest.TestCase):
         self.assertNotIn("object-0", rv)
 
 
-class TestItemListTemplate(unittest.TestCase):
+#class TestItemListTemplate(unittest.TestCase):
+class TestItemListTemplate:
 
-    class TestPage(PageBase):
+    class TestPage:
 
-        plan = [
-            ("info", InfoRegion),
-            ("items", ItemsRegion),
-            ("options", OptionsRegion)]
+        plan = []
 
     def test_definition_list_has_class_and_id(self):
         objects = [
@@ -194,11 +87,12 @@ class TestItemListTemplate(unittest.TestCase):
             6, len(re.findall('<form[^>]+action="/object/[a-f0-9]{32}"', rv)))
 
 
-class TestNavListTemplate(unittest.TestCase):
+#class TestNavListTemplate(unittest.TestCase):
+class TestNavListTemplate:
 
-    class TestPage(PageBase):
+    class TestPage:
 
-        plan = [("nav", NavRegion)]
+        plan = []
 
     def test_nav_section_exists(self):
         p = TestNavListTemplate.TestPage()
@@ -228,11 +122,12 @@ class TestNavListTemplate(unittest.TestCase):
         self.assertIn("pure-menu-selected", rv)
 
 
-class TestOptionListTemplate(unittest.TestCase):
+class TestOptionListTemplate:
+#class TestOptionListTemplate(unittest.TestCase):
 
-    class TestPage(PageBase):
+    class TestPage:
 
-        plan = [("options", OptionsRegion)]
+        plan = []
 
     def test_definition_list_has_class_and_id(self):
         p = TestItemListTemplate.TestPage()
